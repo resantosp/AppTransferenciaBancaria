e3959542d4b7925fb0fa7f09e61ae8338cf72a81
Renata Santos <renatados.santosp@gmail.com>
2022-03-07 22:32:59 -0300

    withdraw and deposit methods

--- TransferenciaBanc/Entities/Account.cs
+++ TransferenciaBanc/Entities/Account.cs
@@ -2,9 +2,51 @@ namespace TransferenciaBanc
 {
     public class Account
     {
-        private AccountType AccountType { get; set; }
-        private double accountBalance { get; set; }
+        private AccountType accountType { get; set; }
+        private double Balance { get; set; }
         private double Credit { get; set; }
-        private double Name { get; set; }
+        private string Name { get; set; }
+
+        //Construct method: it's created in the moment that I create my object
+        public Account(AccountType account_type, double account_balance, double credit, string name)
+        {
+            this.accountType = account_type;
+            this.Balance = account_balance;
+            this.Credit = credit;
+            this.Name = name;
+        }
+
+        public bool Withdraw(double wdAmount)
+        {
+            //Balance validation:
+            //Confirming if my balance + my credit is more than the amount I'm trying to withdraw
+            if (this.Balance - wdAmount < (this.Credit * -1))
+            {
+                System.Console.WriteLine("Insufficient Balance.");
+                return false;
+            }
+
+            this.Balance -= wdAmount;
+            System.Console.WriteLine("Process Finished.");
+            System.Console.WriteLine($"{this.Name}'s Current Balance: {this.Balance}");
+            return true;
+        }
+
+        public void Deposit(double depositAmount)
+        {
+            this.Balance += depositAmount;
+
+            System.Console.WriteLine("Process Finished.");
+            System.Console.WriteLine($"{this.Name}'s Current Balance: {this.Balance}");
+        }
+
+        public void DisplayBalance()
+        {
+            System.Console.WriteLine($"{this.Name}'s Current Balance: {this.Balance}");
+        }
+
+        //Transfer Method (5:33)
     }
+
+    
 }
